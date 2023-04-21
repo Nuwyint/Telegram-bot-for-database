217a1eb6761b6195e3477028f3a3e357adedb830
Nuwyint <sahamain23@gmail.com>
2023-04-22 00:10:52 +0300

    Working bot v.4, revorked all bot, new library, python-telegram-bot

--- ConectToMongodb.py
+++ ConectToMongodb.py
@@ -1,37 +1,111 @@
-from pymongo import *
-
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
 # http://t.me/DataBaseUploadBot
 
-def uploadDataBase(structItem, user_id):
-    cluster = MongoClient("mongodb+srv://Nuwyint:23112004@testfortelegramapi.pcazooy.mongodb.net/telebot?retryWrites=true&w=majority")
+def uploadDataBase(structItem):
+    cluster = MongoClient(
+        "mongodb+srv://Nuwyint:23112004@testfortelegramapi.pcazooy.mongodb.net/telebot?retryWrites=true&w=majority")
     db = cluster["telebot"]
-    print("Conect on")
+    print("Connect on")
 
-    pos = list(map(int, (structItem[user_id]['positions']).split()))
+    pos = list(map(int, (structItem['pos']).split()))
     pos = ({"x": pos[0], "y": pos[1]})
 
     users = {
-        "name": structItem[user_id]['name'],
-        "category": structItem[user_id]['category'],
+        "name": structItem['name'],
         "position": pos,
-        "imgUrl": structItem[user_id]['ImgUrl'],
-        "text": structItem[user_id]['text'],
-        "modelName": structItem[user_id]['modelName'],
+        "imgUrl": structItem['imgUrl'],
+        "text": structItem['textImg'],
+        "modelName": structItem['modelName'],
+        "radius": int(structItem['radius']),
+        "anamorphClass": structItem['anamorthClass'],
     }
 
     db.struct_for_webSite.insert_one(users)  # не забудь здесь поменять коллекции на нужную
 
 
-def uploadNewModel(structItem, user_id):
+def uploadNewModel(structItem):
     cluster = MongoClient(
         "mongodb+srv://Nuwyint:23112004@testfortelegramapi.pcazooy.mongodb.net/telebot?retryWrites=true&w=majority")
     db = cluster["telebot"]
-    print("Conect on")
+    print("Connect on")
 
     model = {
-        "name": structItem[user_id]['name'],
-        "Url": structItem[user_id]['Url'],
+        "name": structItem['nameModel'],
+        "Url": structItem['url'],
     }
 
-    db.strcut.insert_one(model) # не забудь здесь поменять коллекции на нужную
+    db.models.insert_one(model)  # не забудь здесь поменять коллекции на нужную
+
+
+def uploadNewClass(structItem):
+    cluster = MongoClient("mongodb+srv://Nuwyint:23112004@testfortelegramapi.pcazooy.mongodb.net/telebot?retryWrites=true&w=majority")
+    db = cluster["telebot"]
+    print("Connect on")
+
+    _class = {
+        "name": structItem['nameClass'],
+    }
+
+    db.Class.insert_one(_class)  # не забудь здесь поменять коллекции на нужную
+
+
+def downloadModels():
+    cluster = MongoClient("mongodb+srv://Nuwyint:23112004@testfortelegramapi.pcazooy.mongodb.net/telebot?retryWrites=true&w=majority")
+    db = cluster["telebot"]
+    collection = db['models']
+    downloads = collection.find()
+    massOfModelsName = []
+    for download in downloads:
+        if 'name' in download:
+            massOfModelsName.append(download['name'])
+    return sorted(set(massOfModelsName))
+
+
+def downloadRadius():
+    cluster = MongoClient("mongodb+srv://Nuwyint:23112004@testfortelegramapi.pcazooy.mongodb.net/telebot?retryWrites=true&w=majority")
+    db = cluster["telebot"]
+    collection = db['struct_for_webSite']
+    downloads = collection.find()
+    massOfRadius = []
+    for download in downloads:
+        if 'radius' in download:
+            massOfRadius.append(download['radius'])
+    return sorted(set(massOfRadius))
+
+
+def downloadClass():
+    cluster = MongoClient("mongodb+srv://Nuwyint:23112004@testfortelegramapi.pcazooy.mongodb.net/telebot?retryWrites=true&w=majority")
+    db = cluster["telebot"]
+    collection = db['Class']
+    downloads = collection.find()
+    massOfClass = []
+    for download in downloads:
+        if 'name' in download:
+            massOfClass.append(download['name'])
+    return sorted(set(massOfClass))
+
+def downloadDeleteData():
+    cluster = MongoClient("mongodb+srv://Nuwyint:23112004@testfortelegramapi.pcazooy.mongodb.net/telebot?retryWrites=true&w=majority")
+    db = cluster["telebot"]
+    collection = db['struct_for_webSite']
+    downloads = collection.find()
+    massOfClass = []
+    for download in downloads:
+        if 'name' in download:
+            massOfClass.append(download['name'])
+    return sorted(set(massOfClass))
+
+
+def deleteStruct(nameindex):
+    client = MongoClient("mongodb+srv://Nuwyint:23112004@testfortelegramapi.pcazooy.mongodb.net/telebot?retryWrites=true&w=majority")
+    db = client['telebot']
+    collection = db['struct_for_webSite']
+
+    # Укажите фильтр для удаления
+    delete_filter = {"name": str(nameindex)}
+
+    # Удалите один документ, соответствующий фильтру
+    result = collection.delete_one(delete_filter)
+    print(f"Удалено документов: {result.deleted_count}")
--- main.py
+++ main.py
@@ -1,106 +1,237 @@
-import telebot
-from ConectToMongodb import *
+# кнопки
+# поле приоретета
+# кнопки для названия моделей
+# удаление
+# вайтлист
 
-TOKEN = '6022951486:AAFsk95GftNYjSnB2Awl4E0o0zmELIoiLS0'
-bot = telebot.TeleBot(TOKEN)
-print('Bot on')
-structItem = {}
-
-
-@bot.message_handler(commands=['start'])
-def get_text_messages(message):
-    bot.send_message(message.chat.id, "Вот скажи, тебе где нашли в капусте или в караганде? А Привет писать кто будет?")
-@bot.message_handler(func= lambda x : x.text == "Привет" )
-def giveInstructions(message):
-    bot.send_message(message.chat.id, "Привет, коль тебе надобно Михаил, иль хочешь бд обновить, тогда /new пропиши")
+from telegram import Update, ReplyKeyboardMarkup, KeyboardButton, ReplyKeyboardRemove
+from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, CallbackContext
+from ConectToMongodb import uploadDataBase, uploadNewModel, uploadNewClass, downloadModels, downloadRadius, downloadClass, downloadDeleteData, deleteStruct
 
 
-
-@bot.message_handler(commands=['new'])
-def structTake(message):
-    user_id = message.chat.id
-    structItem[user_id] = {"stage": "name"}
-    msg = bot.reply_to(message, 'Введите имя')
-    bot.register_next_step_handler(msg, process_step)
+def start(update: Update, context: CallbackContext):
+    function_keyboard = create_function_keyboard()
+    update.message.reply_text("Привет, я телеграм-бот созданный для заполения и управления mongodb")
+    update.message.reply_text("Пожалуйста, выберите функцию:", reply_markup=function_keyboard)
 
 
-def process_step(message):
-    print("debug: enter in newData")
-
-    user_id = message.chat.id
-    stage = structItem[user_id]['stage']
-
-    if stage == 'name':
-        structItem[user_id].update({"name": message.text, "stage": "category"})
-        msg = bot.reply_to(message, 'category": "...."')
-        print(structItem)
-    elif stage == 'category':
-        structItem[user_id].update({"category": message.text, "stage": "positions"})
-        msg = bot.reply_to(message, 'positions" через пробел координаты: ".. .."')
-        print(structItem)
-    elif stage == 'positions':
-        structItem[user_id].update({"positions": message.text, "stage": "modelName"})
-        msg = bot.reply_to(message, 'modelName": "...."')
-        print(structItem)
-    elif stage == 'modelName':
-        structItem[user_id].update({"modelName": message.text, "stage": "ImgUrl"})
-        msg = bot.reply_to(message, 'ImgUrl": "...."')
-        print(structItem)
-    elif stage == 'ImgUrl':
-        structItem[user_id].update({"ImgUrl": message.text, "stage": "text"})
-        msg = bot.reply_to(message, 'text": "...."')
-        print(structItem)
-    elif stage == 'text':
-        structItem[user_id].update({"text": message.text, "stage": "done"})
-        msg = bot.reply_to(message,
-                           f"Так уж и быть. Получены следующие данные:\n"
-                           f"Имя: {structItem[user_id]['name']}\n"
-                           f"Категория: {structItem[user_id]['category']}\n"
-                           f"Позиция:{structItem[user_id]['positions']}\n"
-                           f"Имя модели:{structItem[user_id]['modelName']}\n"
-                           f"Ссылка на картинку: {structItem[user_id]['ImgUrl']}\n"
-                           f"Доп текст: {structItem[user_id]['text']}")
-        print(structItem)
-        uploadDataBase(structItem, user_id)
-        bot.send_message(message.chat.id, "Данные успешно загружены в базу данных")
-
-    else:
-        msg = bot.reply_to(message, "Введите команду /new, чтобы начать сначала.")
-
-    if structItem[user_id]['stage'] != 'done':
-        bot.register_next_step_handler(msg, process_step)
-
-@bot.message_handler(commands=['newModel'])
-def createModel(message):
-    user_id = message.chat.id
-    structItem[user_id] = {"stage": "name"}
-    msg = bot.reply_to(message, 'Введите имя новой модели')
-    bot.register_next_step_handler(msg, step_model)
+def menu(update: Update, context: CallbackContext):
+    function_keyboard = create_function_keyboard()
+    update.message.reply_text("Пожалуйста, выберите функцию:", reply_markup=function_keyboard)
 
 
-def step_model(message):
-    print("debug: enter in newmodel")
-    user_id = message.chat.id
-    stage = structItem[user_id]['stage']
-
-    if stage == 'name':
-        print("debug: still working in newmodel")
-        structItem[user_id] = {"name": message.text, "stage": "Url"}
-        msg = bot.reply_to(message, 'Url: "....",')
-    elif stage == 'Url':
-        structItem[user_id].update({"Url": message.text, "stage": "done"})
-        msg = bot.reply_to(message,
-                           f"Так уж и быть. Получены следующие данные для новой модели:\n"
-                           f"Имя: {structItem[user_id]['name']}\n"
-                           f"Url:{structItem[user_id]['Url']}\n")
-        print(structItem)
-        uploadNewModel(structItem, user_id)
-        bot.send_message(message.chat.id, "Данные об новой модели успешно загружены в базу данных")
-    else:
-        msg = bot.reply_to(message, "Введите команду /new, чтобы начать сначала.")
-
-    if structItem[user_id]['stage'] != 'done':
-        bot.register_next_step_handler(msg, step_model)
+def create_function_keyboard():
+    keyboard = [
+        [KeyboardButton("Создать новую структуру базы данных"), KeyboardButton("Создать новую модель")], [KeyboardButton("Создать новый класс"), KeyboardButton("Удалить структуру")]
+    ]
+    return ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
 
 
-bot.polling(none_stop=True, interval=0)
+def create_Models_keyboard():
+    listModels = downloadModels()
+    keyboard = []
+    for i in range(0, len(listModels), 2):
+        row = [KeyboardButton(listModels[i])]
+        if i + 1 < len(listModels):
+            row.append(KeyboardButton(listModels[i + 1]))
+        keyboard.append(row)
+    return ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
+
+
+def create_Radius_keyboard():
+    listModels = downloadRadius()
+    keyboard = [[KeyboardButton(func) for func in listModels]]
+    return ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
+
+
+def create_Class_keyboard():
+    listModels = downloadClass()
+    print(listModels)
+    keyboard = []
+    for i in range(0, len(listModels), 2):
+        row = [KeyboardButton(listModels[i])]
+        if i + 1 < len(listModels):
+            row.append(KeyboardButton(listModels[i + 1]))
+        keyboard.append(row)
+    return ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
+
+def create_delete_keyboard():
+    listModels = downloadDeleteData()
+    keyboard = []
+    for i in range(0, len(listModels), 2):
+        row = [KeyboardButton(listModels[i])]
+        if i + 1 < len(listModels):
+            row.append(KeyboardButton(listModels[i + 1]))
+        keyboard.append(row)
+    return ReplyKeyboardMarkup(keyboard, one_time_keyboard=True, resize_keyboard=True)
+
+def newModel(update: Update, context: CallbackContext):
+    update.message.reply_text("Вы начали создание новой модели, чтобы отменить /exit \nНапишите название модели: ")
+
+    context.user_data.pop("name", None)
+    context.user_data.pop("pos", None)
+    context.user_data.pop("imgUrl", None)
+    context.user_data.pop("textImg", None)
+    context.user_data.pop("modelName", None)
+    context.user_data.pop("radius", None)
+    context.user_data.pop("anamorthClass", None)
+    context.user_data.pop("nameModel", None)
+    context.user_data.pop("url", None)
+
+    context.user_data['state'] = "nameModel"
+
+
+def newData(update: Update, context: CallbackContext):
+    update.message.reply_text("Вы начали создание новой структуры объекта, чтобы отменить /exit \nНапишите название объекта: ")
+
+    context.user_data.pop("name", None)
+    context.user_data.pop("pos", None)
+    context.user_data.pop("imgUrl", None)
+    context.user_data.pop("textImg", None)
+    context.user_data.pop("modelName", None)
+    context.user_data.pop("radius", None)
+    context.user_data.pop("anamorthClass", None)
+    context.user_data.pop("nameModel", None)
+    context.user_data.pop("url", None)
+
+    context.user_data['state'] = "name"
+
+
+def newClass(update: Update, context: CallbackContext):
+    update.message.reply_text(
+        "Вы начали создание нового класса, чтобы отменить /exit \nНапишите название класса: ")
+
+    context.user_data.pop("name", None)
+    context.user_data.pop("pos", None)
+    context.user_data.pop("imgUrl", None)
+    context.user_data.pop("textImg", None)
+    context.user_data.pop("modelName", None)
+    context.user_data.pop("radius", None)
+    context.user_data.pop("anamorthClass", None)
+    context.user_data.pop("nameModel", None)
+    context.user_data.pop("url", None)
+    context.user_data.pop('nameClass', None)
+
+    context.user_data['state'] = "nameClass"
+
+
+def process_manager(update: Update, context: CallbackContext):
+    state = context.user_data.get("state")
+    uData = context.user_data
+    text = update.message.text
+    answer = update.message.reply_text
+
+    print(state)
+    if state is not None:
+        # Creating new struct ⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂
+        if state == "name":
+            uData["name"] = text
+            answer('Введите позицию через пробел: ', reply_markup=ReplyKeyboardRemove())
+            uData['state'] = 'pos'
+        elif state == 'pos':
+            uData["pos"] = text
+            answer('Вставте ссылку на фотографию: ')
+            uData['state'] = 'imgUrl'
+        elif state == 'imgUrl':
+            uData['imgUrl'] = text
+            answer('Введите текст: ')
+            uData['state'] = 'textImg'
+        elif state == "textImg":
+            uData['textImg'] = text
+            Models_keyboard = create_Models_keyboard()
+            answer('Выберете модель: ', reply_markup=Models_keyboard)
+            uData['state'] = 'modelName'
+        elif state == 'modelName':
+            Radius_keyboard = create_Radius_keyboard()
+            answer('Выберете радиус: ', reply_markup=Radius_keyboard)
+            uData['modelName'] = text
+            uData['state'] = 'radius'
+        elif state == 'radius':
+            uData['radius'] = text
+            Class_keyboard = create_Class_keyboard()
+            answer('Выберете класс: ', reply_markup=Class_keyboard)
+            uData['state'] = 'anamorthClass'
+        elif state == 'anamorthClass':
+            uData['anamorthClass'] = text
+            uploadDataBase(uData)
+            answer(
+                f'Объект: {uData["name"]}\nПозиция: {uData["pos"]} \nСсылка: {uData["imgUrl"]} \nТекст: {uData["textImg"]}\nМодель: {uData["modelName"]} \nРадиус: {uData["radius"]} \nКласс: {uData["anamorthClass"]}\nбыли сохранены в базе данных.', reply_markup=create_function_keyboard())
+            uData["state"] = None
+            menu(update, context)
+        # creating model ⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂
+        elif state == "nameModel":
+            uData["nameModel"] = text
+            answer('Введите ссылку: ', reply_markup=ReplyKeyboardRemove())
+            uData['state'] = 'url'
+        elif state == 'url':
+            uData["url"] = text
+            uploadNewModel(uData)
+            answer(f'Модель: {uData["nameModel"]}\nСсылка: {uData["url"]} была сохранена в базе данных.')
+            uData["state"] = None
+        # creating "class"⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂
+        elif state == "nameClass":
+            uData['nameClass'] = text
+            uploadNewClass(uData)
+            answer(f'Класс {uData["nameClass"]} инициализирован')
+            uData["state"] = None
+        # deleting ⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂
+        elif state == "death":
+            died = update.message.text
+            deleteStruct(died)
+            update.message.reply_text(f'Структура с названием {died} удалена')
+            died = None
+            menu(update, context)
+        # error ⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂
+        else:
+            print(state)
+            answer('Пожалуйста, используйте команду /start, чтобы начать сбор данных.')
+
+    # Button trigger ⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂
+    elif text == "Создать новую модель":
+        newModel(update, context)
+    elif text == "Создать новую структуру базы данных":
+        newData(update, context)
+    elif text == "Создать новый класс":
+        newClass(update, context)
+    elif text == "Удалить структуру":
+        deleteEleventDb(update, context)
+    # ⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂⌂
+
+
+def deleteEleventDb(update: Update, context: CallbackContext):
+    context.user_data['state'] = "death"
+    update.message.reply_text("Выберете имя по которому хотите удалить структуру", reply_markup = create_delete_keyboard())
+
+
+def deleteNew(update: Update, context: CallbackContext):
+    context.user_data['state'] = None
+    update.message.reply_text('Наполнение отклонено')
+    menu(update, context)
+
+
+def help(update: Update, context: CallbackContext):
+    update.message.reply_text("/start, /menu, /newdatam, /newNodel, /exit, /newClass, /delete, /help")
+
+
+def main():
+    token = '6022951486:AAFsk95GftNYjSnB2Awl4E0o0zmELIoiLS0'
+    updater = Updater(token, use_context=True)
+    print("Bot enable")
+
+    dispatcher = updater.dispatcher
+    dispatcher.add_handler(CommandHandler("start", start))
+    dispatcher.add_handler(CommandHandler("menu", menu))
+    dispatcher.add_handler(CommandHandler("newData", newData))
+    dispatcher.add_handler(CommandHandler('newModel', newModel))
+    dispatcher.add_handler(CommandHandler('exit', deleteNew))
+    dispatcher.add_handler(CommandHandler('newClass', newClass))
+    dispatcher.add_handler(CommandHandler('delete', deleteEleventDb))
+    dispatcher.add_handler(CommandHandler('help', help))
+    dispatcher.add_handler(MessageHandler(Filters.text & ~Filters.command, process_manager))
+
+    updater.start_polling()
+    updater.idle()
+
+
+if __name__ == "__main__":
+    main()
